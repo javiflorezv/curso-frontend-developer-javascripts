37080631e20b5fa93bb135032c206e589efbac92
javiflorezv <javier.florez23@gmail.com>
2023-02-08 16:22:20 -0500

    se agrego al mobile el despliegue del menu oculto

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-menuEmail.addEventListener('click', toggDesktopMenu);
-function toggDesktopMenu(){
-    console.log('JS FUNCIONANDO')
-    desktopMenu.classList.toggle('Inactive');
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
+function toggleDesktopMenu() {
+  desktopMenu.classList.toggle('inactive');
 }
-console.log('funcionando');
+
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle('inactive');
+}
 
 // classList.toggle esta clase desaparece o no siempre y cuando le hasgas click o no
 // actualizando 
+//naaa ya se que fue lo que paso
